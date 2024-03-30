91a2a089bc67b4d5808f78c9bd69101bc3768389
Diego Pereyra <diesphink@gmail.com>
2024-03-30 17:36:42 -0300

    Added thumbnail to files table

--- ui/filestable.py
+++ ui/filestable.py
@@ -8,9 +8,16 @@ class FilesTable(QtWidgets.QTableView):
     def __init__(self):
         super().__init__()
         self.app = QtCore.QCoreApplication.instance()
+        self.delegate = PreviewDelegate()
+
         self.setSelectionMode(QtWidgets.QAbstractItemView.SelectionMode.ExtendedSelection)
         self.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         self.setModel(TableModel())
+        self.setGridStyle(QtCore.Qt.NoPen)
+
+        self.setItemDelegateForColumn(0, self.delegate)
+        self.verticalHeader().setDefaultSectionSize(self.delegate.size)
+        self.verticalHeader().hide()
 
         self.app.filter_changed.connect(self.on_filter_changed)
         self.app.metadata.changed.connect(self.model().layoutChanged.emit)
@@ -60,6 +67,48 @@ class FilesTable(QtWidgets.QTableView):
         event.accept()
         ImportDialog(self, files).exec_()
 
+    @property
+    def size(self):
+        return self.delegate.size
+
+    @size.setter
+    def size(self, value):
+        self.delegate.size = value
+        self.verticalHeader().setDefaultSectionSize(value)
+        self.model().layoutChanged.emit()
+
+
+class PreviewDelegate(QtWidgets.QStyledItemDelegate):
+
+    def __init__(self):
+        super().__init__()
+        self.size = 50
+
+    def paint(self, painter, option, index):
+
+        # data is our preview object
+        data = index.model().data(index, QtCore.Qt.DisplayRole)
+
+        if data is None:
+            return
+
+        image = QtGui.QImage(data)
+
+        scaled = image.scaled(
+            self.size,
+            self.size,
+            QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+        )
+        # Position in the middle of the area.
+        # x = CELL_PADDING + (width - scaled.width()) / 2
+        # y = CELL_PADDING + (height - scaled.height()) / 2
+
+        painter.drawImage(option.rect.x(), option.rect.y(), scaled)
+
+    def sizeHint(self, option, index):
+        # All items the same size.
+        return QtCore.QSize(self.size, self.size)
+
 
 class TableModel(QtCore.QAbstractTableModel):
 
@@ -84,12 +133,14 @@ class TableModel(QtCore.QAbstractTableModel):
         if role == QtCore.Qt.ItemDataRole.DisplayRole:
             if orientation == QtCore.Qt.Orientation.Horizontal:
                 if section == 0:
-                    return "Name"
+                    return ""
                 elif section == 1:
-                    return "Collection"
+                    return "Name"
                 elif section == 2:
-                    return "Tags"
+                    return "Collection"
                 elif section == 3:
+                    return "Tags"
+                elif section == 4:
                     return "Path"
             else:
                 return section + 1
@@ -99,41 +150,41 @@ class TableModel(QtCore.QAbstractTableModel):
         return len(self.files)
 
     def columnCount(self, index):
-        return 4
+        return 5
 
     def data(self, index, role):
-
         if role == QtCore.Qt.ItemDataRole.DisplayRole or role == QtCore.Qt.ItemDataRole.EditRole:
             if index.column() == 0:
-                return self.files[index.row()].name
+                return self.files[index.row()].thumbnail_file
             elif index.column() == 1:
-                return self.files[index.row()].collection.name
+                return self.files[index.row()].name
             elif index.column() == 2:
-                return ", ".join(self.files[index.row()].tags)
+                return self.files[index.row()].collection.name
             elif index.column() == 3:
+                return ", ".join(self.files[index.row()].tags)
+            elif index.column() == 4:
                 return self.files[index.row()].path
 
     def flags(self, index):
         flags = QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled
-        if index.column() == 0 or index.column() == 2:
+        if index.column() == 1 or index.column() == 3:
             flags |= QtCore.Qt.ItemIsEditable
         return flags
 
     def validate(self, index, value):
-        print(value)
-        if index.column() == 0:
+        if index.column() == 1:
             return value.strip() != ""
-        if index.column() == 2:
+        if index.column() == 3:
             return True
         return False
 
     def setData(self, index, value, role):
         if role == QtCore.Qt.EditRole:
             app = QtCore.QCoreApplication.instance()
-            if index.column() == 0:
+            if index.column() == 1:
                 if not self.validate(index, value):
                     return False
                 app.metadata.update_file(self.files[index.row()], name=value)
-            if index.column() == 2:
+            if index.column() == 3:
                 app.metadata.update_file(self.files[index.row()], tags=tags_from_text(value))
             return True
